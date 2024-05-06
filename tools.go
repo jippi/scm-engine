b8d7b2a5193de2dae1c1830fbae1f13420adf363
Christian Winther <jippignu@gmail.com>
2024-05-06 22:13:05 +0200

    fix: tidy?

--- tools.go
+++ tools.go
@@ -6,6 +6,7 @@ package tools
 import (
 	_ "github.com/99designs/gqlgen"
 	_ "github.com/davecgh/go-spew/spew"
-	_ "github.com/fatih/structtag"    // needed for go:generate
-	_ "github.com/iancoleman/strcase" // needed for go:generate
+	_ "github.com/fatih/structtag"         // needed for go:generate
+	_ "github.com/iancoleman/strcase"      // needed for go:generate
+	_ "github.com/vektah/gqlparser/v2/ast" // needed for go:generate
 )
