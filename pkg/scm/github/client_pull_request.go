1fe22f8be3d9bfe455d08218069b212c79a3c89a
Christian Winther <jippignu@gmail.com>
2024-05-21 21:26:20 +0200

    ci: silence nilnil check

--- pkg/scm/github/client_pull_request.go
+++ pkg/scm/github/client_pull_request.go
@@ -54,9 +54,9 @@ func (client *MergeRequestClient) Update(ctx context.Context, opt *scm.UpdateMer
 }
 
 func (client *MergeRequestClient) GetRemoteConfig(ctx context.Context, filename, ref string) (io.Reader, error) {
-	return nil, nil
+	return nil, nil //nolint:nilnil
 }
 
 func (client *MergeRequestClient) List(ctx context.Context, options *scm.ListMergeRequestsOptions) ([]scm.ListMergeRequest, error) {
-	return nil, nil
+	return nil, nil //nolint:nilnil
 }
