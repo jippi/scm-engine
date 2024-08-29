39e939a4c46a83c5040bee24041b855c6a0875aa
Christian Winther <jippignu@gmail.com>
2024-08-29 13:58:48 +0200

    chore: pass in Now to updatedWithinDuration

--- pkg/scm/gitlab/context_merge_request.go
+++ pkg/scm/gitlab/context_merge_request.go
@@ -63,8 +63,8 @@ func (e ContextMergeRequest) HasActivityWithin(ctx context.Context, input any) b
 }
 
 // updatedWithinDuration checks if the MR has been updated within the provided duration
-func (e ContextMergeRequest) updatedWithinDuration(dur time.Duration) bool {
-	return time.Now().Sub(e.UpdatedAt) < dur
+func (e ContextMergeRequest) updatedWithinDuration(now time.Time, dur time.Duration) bool {
+	return now.Sub(e.UpdatedAt) < dur
 }
 
 // has_any_activity_within
@@ -74,7 +74,7 @@ func (e ContextMergeRequest) HasAnyActivityWithin(ctx context.Context, input any
 	cfg := config.FromContext(ctx)
 
 	// If the MR UpdatedAt has been updated within the duration, then we got some kind of activity
-	if e.updatedWithinDuration(dur) {
+	if e.updatedWithinDuration(now, dur) {
 		return true
 	}
 
