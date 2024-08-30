b01bf9bd919cf6dcfeb567255aaaf82e4ba4c89b
Christian Winther <jippignu@gmail.com>
2024-08-30 17:37:49 +0200

    chore: cleanup error handler so 404 errors are not logged _as_ errors

--- cmd/gitlab_server_helpers.go
+++ cmd/gitlab_server_helpers.go
@@ -4,12 +4,18 @@ import (
 	"context"
 	"log/slog"
 	"net/http"
+	"strings"
 
 	slogctx "github.com/veqryn/slog-context"
 )
 
 func errHandler(ctx context.Context, w http.ResponseWriter, code int, err error) {
-	slogctx.Error(ctx, "Server response", slog.Int("response_code", code), slog.Any("response_message", err))
+	// Treat 404 errors as informational instead of actual errors
+	if strings.Contains(err.Error(), "404 Not Found") {
+		slogctx.Info(ctx, "Server response", slog.Int("response_code", code), slog.Any("response_message", err))
+	} else {
+		slogctx.Error(ctx, "Server response", slog.Int("response_code", code), slog.Any("response_message", err))
+	}
 
 	w.WriteHeader(code)
 	w.Write([]byte(err.Error()))
