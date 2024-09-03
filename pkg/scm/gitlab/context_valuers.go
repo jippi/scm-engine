7a327e76ac4c9717a0c115235e35656d4a75fd00
Christian Winther <jippignu@gmail.com>
2024-09-03 16:42:36 +0200

    feat: make enums act as proper strings in Expr-lang scripts

---
+++ pkg/scm/gitlab/context_valuers.go
@@ -0,0 +1,27 @@
+package gitlab
+
+// Extending the EvalContext with Expr-lang "valuer" interface support
+// allowing custom types to act as native Expr-lang types
+//
+// See: https://github.com/expr-lang/expr/blob/master/patcher/value/value.go
+// See: pkg/stdlib/stdlib.go
+
+// MergeRequestState is a ENUM type
+func (d MergeRequestState) AsString() string {
+	return d.String()
+}
+
+// UserState is a ENUM type
+func (d UserState) AsString() string {
+	return d.String()
+}
+
+// MergeStatus is a ENUM type
+func (d MergeStatus) AsString() string {
+	return d.String()
+}
+
+// DetailedMergeStatus is a ENUM type
+func (d DetailedMergeStatus) AsString() string {
+	return d.String()
+}
--- pkg/stdlib/stdlib.go
+++ pkg/stdlib/stdlib.go
@@ -2,11 +2,19 @@ package stdlib
 
 import (
 	"github.com/expr-lang/expr"
+	"github.com/expr-lang/expr/patcher/value"
 )
 
 var Functions = []expr.Option{
 	// Replace built-in duration function with one that supports "d" (days) and "w" (weeks)
 	expr.DisableBuiltin("duration"),
+
+	// Add Expr-lang support for a wider range of "valuers" for custom types, such as
+	//
+	// - "AsString()" interface for custom types wanting to be used as a String (useful for Enum types!)
+	// - "AsBool()" interface for custom types wanting to be used as a boolean comparison
+	value.ValueGetter,
+
 	Duration,
 	Since,
 
