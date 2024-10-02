7fc171e5fff4aa159ca38ea55f7da78f53f373d5
Brianna Becker <bbecker@seatgeek.com>
2024-10-02 23:44:26 +0200

    Pass ID to actor

--- pkg/scm/gitlab/context_user.go
+++ pkg/scm/gitlab/context_user.go
@@ -4,6 +4,7 @@ import "github.com/jippi/scm-engine/pkg/scm"
 
 func (u ContextUser) ToActor() scm.Actor {
 	return scm.Actor{
+		ID:       u.ID,
 		Username: u.Username,
 		IsBot:    u.Bot,
 		Email:    u.PublicEmail,
