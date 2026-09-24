b014848bee30b819ec3c211c69149fb0f9a962d5
Christian Winther <jippignu@gmail.com>
2026-09-25 01:15:41 +0200

    fix(github): create conversation comments with issue API

--- pkg/scm/github/client_actioner.go
+++ pkg/scm/github/client_actioner.go
@@ -103,8 +103,8 @@ func (c *Client) ApplyStep(ctx context.Context, evalContext scm.EvalContext, upd
 			return nil
 		}
 
-		_, _, err = c.wrapped.PullRequests.CreateComment(ctx, owner, repo, state.MergeRequestIDInt(ctx), &go_github.PullRequestComment{
-			Body: scm.Ptr(msg),
+		_, _, err = c.wrapped.Issues.CreateComment(ctx, owner, repo, state.MergeRequestIDInt(ctx), go_github.IssueCommentRequest{
+			Body: msg,
 		})
 
 		return err
